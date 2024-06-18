4f3228ace8e1b43a0d3426ae67e15e850c900390
yama998877 <81412896+yama998877@users.noreply.github.com>
2024-06-18 11:58:06 +0900

    Delete vendor/bundle/ruby/3.0.0 directory
    不要なので削除

--- vendor/bundle/ruby/3.0.0/gems/rubocop-fjord-0.3.0/lib/rubocop/fjord.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'rubocop/fjord/version'
-
-module Rubocop
-  module Fjord
-    class Error < StandardError; end
-  end
-end
--- vendor/bundle/ruby/3.0.0/gems/sinatra-contrib-3.0.6/lib/sinatra/contrib/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Sinatra
-  module Contrib
-    VERSION = '3.0.6'
-  end
-end
